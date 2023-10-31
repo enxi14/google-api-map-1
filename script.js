ce9d73c08bedadb791089522e05d234f34efc9f7
enxi14 <36089987+enxi14@users.noreply.github.com>
2023-10-31 17:37:50 -0400

    commit

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ async function initMap() {
 const { Map } = await google.maps.importLibrary("maps");
 
 map = new Map(document.getElementById("map"), {
-    center: { lat: 35.668839, lng: -80.472081 },
+    center: { lat: 35.6683179, lng: -80.4721204 },
     zoom: 8,
 });
 }
