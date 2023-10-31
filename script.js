46c44c072777f6e8fbd37fea0a1bc3a1ed448034
enxi14 <36089987+enxi14@users.noreply.github.com>
2023-10-31 17:50:04 -0400

    commit

--- script.js
+++ script.js
@@ -1,40 +1,38 @@
 let map;
-let parkCircle; // This will store the circle overlay
+
 
 async function initMap() {
-const { Map, Circle } = await google.maps.importLibrary("maps");
+const { Map, LatLngBounds } = await google.maps.importLibrary("maps");
 
 map = new Map(document.getElementById("map"), {
     center: { lat: 35.6683179, lng: -80.4721204 },
     zoom: 8,
 });
 
-  // Define the circle properties
-parkCircle = new Circle({
-    center: { lat: 35.6683179, lng: -80.4721204 }, // Replace with the park's coordinates
-    radius: 5000, // Radius in meters (adjust as needed)
-    strokeColor: "#FF0000", // Color of the circle border
-    strokeOpacity: 0.8,
-    strokeWeight: 2,
-    fillColor: "#FF0000", // Color of the circle fill
-    fillOpacity: 0.35,
+  // Create an array of marker positions (latitude and longitude)
+const markerPositions = [
+    { lat: 35.6683179, lng: -80.4721204 }, // Example marker 1
+    { lat: 35.7, lng: -80.5 }, // Example marker 2
+    // Add more markers as needed
+];
+
+  // Create an empty bounds object
+const bounds = new LatLngBounds();
+
+  // Loop through marker positions and add markers to the map
+markerPositions.forEach((position) => {
+    new google.maps.Marker({
+    position: position,
+    map: map,
+    title: 'Marker Title'
+    });
+
+    // Extend the bounds to include the marker
+    bounds.extend(position);
 });
 
-  parkCircle.setMap(map); // Add the circle to the map
+  // Automatically adjust the map's zoom level to fit all markers
+map.fitBounds(bounds);
 }
 
-function zoomIn() {
-if (map) {
-    const currentZoom = map.getZoom();
-    map.setZoom(currentZoom + 1);
-}
-}
-
-function zoomOut() {
-if (map) {
-    const currentZoom = map.getZoom();
-    map.setZoom(currentZoom - 1);
-}
-}
-
-
+initMap();
