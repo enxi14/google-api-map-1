0b0c9710a2a8f2e20a934da4419c2c6ca5504bed
enxi14 <36089987+enxi14@users.noreply.github.com>
2023-10-31 17:45:05 -0400

    commit

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
 let map;
+let parkCircle; // This will store the circle overlay
 
 async function initMap() {
-const { Map } = await google.maps.importLibrary("maps");
+const { Map, Circle } = await google.maps.importLibrary("maps");
 
 map = new Map(document.getElementById("map"), {
     center: { lat: 35.6683179, lng: -80.4721204 },
     zoom: 8,
 });
+
+  // Define the circle properties
+parkCircle = new Circle({
+    center: { lat: 35.6683179, lng: -80.4721204 }, // Replace with the park's coordinates
+    radius: 5000, // Radius in meters (adjust as needed)
+    strokeColor: "#FF0000", // Color of the circle border
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: "#FF0000", // Color of the circle fill
+    fillOpacity: 0.35,
+});
+
+  parkCircle.setMap(map); // Add the circle to the map
 }
 
-initMap();
+function zoomIn() {
+if (map) {
+    const currentZoom = map.getZoom();
+    map.setZoom(currentZoom + 1);
+}
+}
+
+function zoomOut() {
+if (map) {
+    const currentZoom = map.getZoom();
+    map.setZoom(currentZoom - 1);
+}
+}
+
+
