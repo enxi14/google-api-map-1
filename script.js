44f24d944dd8747dc1f771024f5abe7ae5aaaa78
enxi14 <36089987+enxi14@users.noreply.github.com>
2023-11-03 17:17:21 -0400

    commit

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 let map;
 
-
 async function initMap() {
-const { Map, LatLngBounds } = await google.maps.importLibrary("maps");
+  const { Map, LatLngBounds } = await google.maps.importLibrary("maps");
 
-map = new Map(document.getElementById("map"), {
+  map = new Map(document.getElementById("map"), {
     center: { lat: 35.6683179, lng: -80.4721204 },
-    zoom: 8,
-});
+    // No zoom property to let it auto-adjust
+  });
 
   // Create an array of marker positions (latitude and longitude)
-const markerPositions = [
+  const markerPositions = [
     { lat: 35.6683179, lng: -80.4721204 }, // Example marker 1
     { lat: 35.7, lng: -80.5 }, // Example marker 2
     // Add more markers as needed
-];
+  ];
 
   // Create an empty bounds object
-const bounds = new LatLngBounds();
+  const bounds = new LatLngBounds();
 
   // Loop through marker positions and add markers to the map
-markerPositions.forEach((position) => {
+  markerPositions.forEach((position) => {
     new google.maps.Marker({
-    position: position,
-    map: map,
-    title: 'Marker Title'
+      position: position,
+      map: map,
+      title: 'Marker Title'
     });
 
     // Extend the bounds to include the marker
     bounds.extend(position);
-});
+  });
 
   // Automatically adjust the map's zoom level to fit all markers
-map.fitBounds(bounds);
+  map.fitBounds(bounds);
 }
 
 initMap();
+
